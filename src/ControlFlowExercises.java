7420a7dbfa44a580960bc7e5489fc70b8fa3822c
RodgerScott <rodger.scott@gmail.com>
2018-02-27 16:43:06 -0600

    Completed the a.While exercise.

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,10 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int someNumber = 5;
+
+        while (someNumber <= 15) {
+            System.out.print(someNumber + " ");
+            someNumber +=1;
+        }
+    }
+}
