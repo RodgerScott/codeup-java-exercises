84b6dc3e00ed0f2c93f95f44d5ff767452884f2d
RodgerScott <rodger.scott@gmail.com>
2018-02-27 12:03:56 -0600

    Completed syntax, types, and variables exercise.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,57 @@
+import com.sun.source.util.SourcePositions;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 13;
+
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string.";
+
+        System.out.println(myString);
+
+        char newValue = 'c';
+
+//        myString = (char) newValue;
+
+        double testNumber = 3.14159;
+
+//        myString = (double) testNumber;
+
+        double myNumber = 3.14;
+
+        System.out.println(myNumber);
+
+//        int x = 5;
+//
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+//        int x = 4;
+//        x += 5;
+//
+//        System.out.println(x);
+
+
+//        int x = 3;
+////        int y = 4;
+////        y *= x;
+////
+////        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+//        x /= y;
+
+        y -= x;
+
+        System.out.println(y);
     }
 }
