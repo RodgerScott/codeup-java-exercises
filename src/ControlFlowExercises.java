992531317cfca17731d9cbd55d9c91c9e59e0a1e
RodgerScott <rodger.scott@gmail.com>
2018-02-27 16:49:59 -0600

    Completed the b. Do While exercise.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,24 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int someNumber = 5;
+//        int counter = 0;
 
-        while (someNumber <= 15) {
-            System.out.print(someNumber + " ");
-            someNumber +=1;
-        }
+//        do {
+//            System.out.println(counter);
+//            counter += 2;
+//        } while (counter <= 100);
+
+//        int counter = 100;
+////
+////        do {
+////            System.out.println(counter);
+////            counter -= 5;
+////        } while (counter >= -10);
+
+        long counter = 2;
+
+        do {
+            System.out.println(counter);
+            counter *= counter;
+        } while (counter <= 1000000);
     }
 }
