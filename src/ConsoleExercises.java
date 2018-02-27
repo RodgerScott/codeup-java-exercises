8fc6dfdbdece0ca03ea97cff328a4d111625c38f
RodgerScott <rodger.scott@gmail.com>
2018-02-27 15:24:42 -0600

    Finished exercises and bonus.

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+
+//Exercise 1.
+//        int userInput;
+//        System.out.print("Please enter a number: ");
+//        userInput = ip.nextInt();
+//
+//        System.out.println("You entered: \"" + userInput + "\"");
+
+//Exercise 2.
+//        String wordOne;
+//        String wordTwo;
+//        String wordThree;
+//
+//        System.out.print("Please enter 3 words: ");
+//        wordOne = ip.next();
+//        wordTwo = ip.next();
+//        wordThree = ip.next();
+//
+//        System.out.println("Your first word was: " + wordOne);
+//        System.out.println("You second word was: " + wordTwo);
+//        System.out.println("You third word was: " + wordThree);
+
+//Exercise 3.
+//        String newUserInput;
+//
+//        System.out.print("Please enter a sentence: ");
+//
+//        newUserInput = ip.nextLine();
+//
+//        System.out.println("Your sentence was: " + newUserInput);
+
+//Exercise 4.
+
+    double length;
+    double width;
+    double height;
+
+        System.out.print("Please enter the length, in feet: ");
+        length = ip.nextDouble();
+        System.out.print("Please enter the width, in feet: ");
+        width = ip.nextDouble();
+        System.out.print("Please enter the height, in feet: ");
+        height = ip.nextDouble();
+
+
+        System.out.println("The area of the classroom is: " + (length * width) + "ft");
+        System.out.println("The perimeter of the classroom is: " + ((2 * length) + (2 * width)) + "ft");
+        System.out.println("The volume of the classroom is: " + (length * width * height) + "ft");
+
+    }
+}
