88cb5a2609ba65781007d84780ec41db0f128bb3
RodgerScott <rodger.scott@gmail.com>
2018-02-28 11:04:42 -0600

    bonuses completed

--- src/JavaConsoleIOBonuses.java
+++ src/JavaConsoleIOBonuses.java
@@ -10,7 +10,7 @@ public class JavaConsoleIOBonuses {
 // Ask them to enter how many words are in the sentence
 // Output the number they entered
 
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in).useDelimiter(",");
 
 //        System.out.println("Please enter your favorite quote below: ");
 //        String quote = scan.nextLine();
@@ -33,14 +33,14 @@ public class JavaConsoleIOBonuses {
 // 2) SecondFood
 // 3) ThirdFood
 
-        System.out.print("Please enter your 3 favorite foods: ");
-        String firstFood = scan.next();
-        String secondFood = scan.next();
-        String thirdFood = scan.next();
-
-        System.out.printf("1) %s%n", firstFood);
-        System.out.printf("2) %s%n", secondFood);
-        System.out.printf("3) %s%n", thirdFood);
+//        System.out.print("Please enter your 3 favorite foods: ");
+//        String firstFood = scan.next();
+//        String secondFood = scan.next();
+//        String thirdFood = scan.next();
+//
+//        System.out.printf("1) %s%n", firstFood);
+//        System.out.printf("2) %s%n", secondFood);
+//        System.out.printf("3) %s%n", thirdFood);
 
 
 
@@ -49,5 +49,14 @@ public class JavaConsoleIOBonuses {
 // Each item should only be separated by a comma (no spaces)
 // You will need to use the .useDelimiter() method on your scanner object
 // Output the result as a comma-separated list using printf()
+        System.out.print("Please enter a grocery list of 3 items: ");
+        String first = scan.next();
+        String second = scan.next();
+        String third = scan.next();
+
+        System.out.printf("%s,", first);
+        System.out.printf("%s,", second);
+        System.out.printf("%s,", third);
+
     }
 }
