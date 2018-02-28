58f3f06404d16e6b5272625adc856f1784e114a6
RodgerScott <rodger.scott@gmail.com>
2018-02-28 10:33:33 -0600

    working on bonuses

--- src/ControlStructuresBonus.java
+++ src/ControlStructuresBonus.java
@@ -56,15 +56,35 @@ public class ControlStructuresBonus {
 //  3. Prompt the user to enter an integer and output all the days of the week that
 //  have no more letters than the integer given.
 
-        System.out.print("Please pick an integer: ");
-        int selection = scan.nextInt();
-        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+//        System.out.print("Please pick an integer: ");
+//        int selection = scan.nextInt();
+//        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+//
+//        for (int i = 0; i < days.length; i++) {
+//            if (selection >= days[i].length()) {
+//                System.out.println(days[i]);
+//            }
+//        }
 
-        for (int i = 0; i < days.length; i++) {
-            if (selection >= days[i].length()) {
-                System.out.println(days[i]);
-            }
+
+//  4. Prompt the user to enter their name. Output whether the name is
+//          - short (less than 4 characters), medium (less than 9), long (more than 8)
+//  - contains the letter 'y'
+//          - is a palindrome
+
+        System.out.print("Please enter your name: ");
+        String enteredName = scan.nextLine();
+
+        if (enteredName.length() < 4) {
+            System.out.println("You have a very short name.");
         }
-
+        else if (enteredName.length() < 8) {
+            System.out.println("You have a medium-sized name:");
+        } else if (enteredName.length() >= 9) {
+            System.out.println("You have a long name.");
+        }
+        if (enteredName.contains("y") ) {
+            System.out.println("You have a \"y\" in your name.");
+        }
     }
 }
