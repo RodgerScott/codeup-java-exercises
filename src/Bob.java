e15ed331b7f78dfe559ff14a9aa94b91cc7ed5d6
RodgerScott <rodger.scott@gmail.com>
2018-02-28 14:17:05 -0600

    Completed Bob exercise.

---
+++ src/Bob.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Communicate with Bob: ");
+        String comm = scan.nextLine();
+
+        if (comm.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        }
+        else if (comm.endsWith("?")) {
+            System.out.println("Sure");
+        }
+        else if (comm.equals("")) {
+            System.out.println("Fine! Be that way!");
+        } else {
+            System.out.println("Whatever");
+        }
+    }
+}
