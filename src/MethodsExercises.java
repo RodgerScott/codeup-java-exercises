57707781d7aee32d33f0fff6eed9862019a76a1f
RodgerScott <rodger.scott@gmail.com>
2018-03-01 09:05:22 -0600

    working on bonuses

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -10,6 +10,8 @@ public class MethodsExercises {
 //        System.out.println(multiplication(10,5));
 //        System.out.println(division(10, 2));
 //        System.out.println(remainder(9,2));
+//        System.out.println(factorial(2));
+        getFactorial();
 
     }
 
@@ -40,5 +42,47 @@ public class MethodsExercises {
 
 //2.
 
+    public static long factorial(long a) {
+    if (a == 1) {
+        return 1;
+    }
+    return a * (factorial(a-1));
+    }
+
+
+    public static void getFactorial() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter a number between 1 and 10: ");
+        long entry = scan.nextInt();
+
+        if (entry < 1 || entry > 10) {
+            System.out.println("Sorry, that is not a valid number.");
+            getFactorial();
+        }
+
+        else {
+
+        }
+
+        System.out.println(factorial(entry));
+
+        System.out.println();
+
+        System.out.println("Would you like to play again? y/n");
+        String keepGoing = scan.nextLine();
+        scan.nextLine();
+        System.out.println();
+
+        if (keepGoing.equalsIgnoreCase("y")) {
+            getFactorial();
+            scan.nextLine();
+        }
+        else {
+            System.out.println("Goodbye");
+        }
+
+    }
 
 }
+
