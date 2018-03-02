29c015ccce65819e8702aa82feb0abc4f9d8ec42
RodgerScott <rodger.scott@gmail.com>
2018-03-02 13:20:59 -0600

    Completed understanding references.

--- src/Person.java
+++ src/Person.java
@@ -19,12 +19,37 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person person1 = new Person("Geoff");
+        Person person0 = new Person("Geoff");
 
+        System.out.println(person0.getName());
+
+        person0.setName("Steve");
+
+        person0.sayHello();
+
+// Understanding References:
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//
+//        System.out.println(person1.getName().equals(person2.getName()));
+        //True
+//        System.out.println(person1 == person2);
+        //False
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+        //True
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        Person person3 = person2;
         System.out.println(person1.getName());
-
-        person1.setName("Steve");
-
-        person1.sayHello();
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        System.out.println(person3.getName());
     }
 }
