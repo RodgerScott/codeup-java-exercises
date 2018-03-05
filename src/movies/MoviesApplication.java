dd851f9394a19db2dde8b0271e85c0fbcb1a3cb4
RodgerScott <rodger.scott@gmail.com>
2018-03-05 12:04:25 -0600

    working on movies exercise.

---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,50 @@
+package movies;
+
+import java.util.Scanner;
+import util.Input;
+
+public class MoviesApplication {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int choice;
+
+        System.out.println("What would you like to do?");
+        System.out.println();
+        System.out.println("0 - exit");
+        System.out.println("1 - view all movies");
+        System.out.println("2 - view movies in the animated category");
+        System.out.println("3 - view movies in the drama category");
+        System.out.println("4 - view movies in the horror category");
+        System.out.println("5 - view movies in the sci-fi category");
+        System.out.println();
+        System.out.println("Enter your choice: ");
+        choice = scan.nextInt();
+        scan.nextLine();
+
+        switch (choice) {
+            case 0:
+                break;
+            case 1:
+                System.out.println("All Movies");
+                break;
+            case 2:
+                System.out.println("Animated");
+                break;
+            case 3:
+                System.out.println("drama");
+                break;
+            case 4:
+                System.out.println("horror");
+                break;
+            case 5:
+                System.out.println("sci-fi");
+                break;
+        }
+
+
+
+
+    }
+}
