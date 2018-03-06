698f71cccd806312fa4e86e2f578ea60b959ba3e
RodgerScott <rodger.scott@gmail.com>
2018-03-06 15:12:50 -0600

    Completed polymorphism exercise.

---
+++ src/shapes/Square.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public class Square extends Rectangle {
+
+    public Square(int side) {
+        super(side, side);
+    }
+
+    double getArea() {
+        return super.getArea();
+    }
+
+    double getPerimeter() {
+        return super.getPerimeter();
+    }
+}
