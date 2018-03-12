fe7f12f14d9a710bd514267e5b85bd248defd25c
RodgerScott <rodger.scott@gmail.com>
2018-03-12 13:52:07 -0500

    Completed exercises and bonuses.

--- src/util/FileHelper.java
+++ src/util/FileHelper.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 public class FileHelper {
 
     public static List<String> slurp(String filepath) {
@@ -57,14 +59,53 @@ public class FileHelper {
         }
     }
 
+    public static void copy(String originalFileName, String copyName) {
+        try {
+            Files.copy(Paths.get(originalFileName), Paths.get(copyName), REPLACE_EXISTING);
+
+        } catch(Exception e) {
+            System.out.println(e.fillInStackTrace());
+        }
+    }
+
+    public static void move(String oldPath, String newPath) {
+        try{
+            Files.move(Paths.get(oldPath), Paths.get(newPath));
+        } catch(Exception e) {
+            System.out.println(e.fillInStackTrace());
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Please enter in a file name: ");
-        String entry = scan.nextLine();
-        System.out.println();
-        System.out.println("Please enter some contents: ");
-        List<String> revoList = Arrays.asList(scan.nextLine());
-        spit(entry, revoList);
+//        System.out.println("Please enter a filepath: ");
+//        String filepath = scan.nextLine();
+//        System.out.println(slurp(filepath));
+
+//        System.out.println("Please enter in a file name: ");
+//        String entry = scan.nextLine();
+//        System.out.println();
+//        System.out.println("Please enter some contents: ");
+//        List<String> revoList = Arrays.asList(scan.nextLine());
+//        spit(entry, revoList);
+
+//        System.out.println("Please enter in the filepath to make exciting: ");
+//        String exciting = scan.nextLine();
+//        makeExciting(exciting);
+
+//        System.out.println("Please enter and old file name: ");
+//        String oldName = scan.nextLine();
+//        System.out.println();
+//        System.out.println("Please enter in the new file name: ");
+//        String newName = scan.nextLine();
+//        copy(oldName, newName);
+
+//        System.out.println("Please enter the old file path: ");
+//        String oldFilePath = scan.nextLine();
+//        System.out.println();
+//        System.out.println("Please enter the new file path: ");
+//        String newFilePath = scan.nextLine();
+//        move(oldFilePath, newFilePath);
     }
 }
